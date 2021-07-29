37198f63fb8f732d77aac6c6e4d381c1a7e14a84
Mauricio Paternina <spaceinvadev@gmail.com>
2021-07-29 17:41:53 -0500

    add computed property to sort submission posts

---
+++ upvote/app/main.js
@@ -0,0 +1,14 @@
+const upvoteApp = {
+  data() {
+    return {
+      submissions: Seed.submissions
+    }
+  },
+  computed: {
+    sortedSubmissions() {
+      return this.submissions.sort((a, b) => b.votes - a.votes)
+    }
+  }
+}
+
+Vue.createApp(upvoteApp).mount('#app')
